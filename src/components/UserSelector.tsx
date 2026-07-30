945727d7cc00d1e249202d149805ea72abe4d6af
Vladyslav <vkobeluh@gmail.com>
2026-07-30 08:25:37 +0300

    feat: fetch and display the users

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import { useState } from 'react';
+import { User } from '../types/User';
 
-export const UserSelector: React.FC = () => {
+interface Props {
+  users: User[];
+}
+
+export const UserSelector = ({ users }: Props) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div data-cy="UserSelector" className="dropdown is-active">
       <div className="dropdown-trigger">
@@ -9,6 +20,7 @@ export const UserSelector: React.FC = () => {
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
+          onClick={handleDropdownToggle}
         >
           <span>Choose a user</span>
 
@@ -18,25 +30,21 @@ export const UserSelector: React.FC = () => {
         </button>
       </div>
 
-      <div className="dropdown-menu" id="dropdown-menu" role="menu">
-        <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">
-            Leanne Graham
-          </a>
-          <a href="#user-2" className="dropdown-item is-active">
-            Ervin Howell
-          </a>
-          <a href="#user-3" className="dropdown-item">
-            Clementine Bauch
-          </a>
-          <a href="#user-4" className="dropdown-item">
-            Patricia Lebsack
-          </a>
-          <a href="#user-5" className="dropdown-item">
-            Chelsey Dietrich
-          </a>
+      {isDropdownOpen && (
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            {users.map((user, index) => (
+              <a
+                key={user.id}
+                href={`#user-${index}`}
+                className="dropdown-item"
+              >
+                {user.name}
+              </a>
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
